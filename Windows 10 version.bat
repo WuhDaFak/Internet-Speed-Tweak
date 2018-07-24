5f0483962c8074489d735e113d4784e4c36ae61b
Aaron Armstrong <aaron1056@gmail.com>
2018-07-24 10:19:15 -0500

    u

--- Windows 10 version.bat
+++ Windows 10 version.bat
@@ -1,7 +1,7 @@
 color 17
 echo off
 cls
-echo Windows 10 edition V0.2 
+echo Windows 10 version
 echo Run as administrator for this to work!!!
 set /p q1=Do you want to do a ping test?-
 if "%q1%" == "yes" goto :yes
@@ -20,6 +20,8 @@ exit
 cls
 netsh int tcp set global autotuninglevel=normal
 netsh int tcp set global fastopen=enable
+netsh int tcp set supplemental internet congestionprovider=ctcp
+
 echo ________________________________________________________________________________
 echo You should restart your computer to apply settings.
 pause
@@ -27,9 +29,11 @@ cls
 echo Here are the changes made...
 netsh int tcp show global
 echo ............
+netsh int tcp show supplemental
 echo ............
 echo 1: "netsh int tcp set global autotuninglevel=normal"
 echo 2: "netsh int tcp set global fastopen=enable"
+echo 3: "netsh int tcp set supplemental internet congestionprovider=ctcp"
 pause
 cls
 exit
